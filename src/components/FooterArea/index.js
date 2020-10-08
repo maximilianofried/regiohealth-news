97ad633369884bbc10bf672d90fe3e00c3aced3b
maximilianofried <maximilianofried@gmail.com>
2020-10-08 19:21:49 +0200

    adds eslint and prettier config, starts refactoring code

--- src/components/FooterArea/index.js
+++ src/components/FooterArea/index.js
@@ -1,170 +1,100 @@
-import React, {useState} from 'react';
-import {Link} from "react-router-dom";
-import {createBrowserHistory} from 'history';
-import FooterCopyright from "../FooterCopyright";
-import FooterMoreNews from "../FooterMoreNews";
-import TwitterFeed from "../TwitterFeed";
-import FontAwesome from "../uiStyle/FontAwesome";
-import {ReactComponent as GtLogo} from '../../doc/img/gt-logo/logo-gt.svg';
-import {ReactComponent as GtLogo2} from '../../doc/img/gt-logo/GT-PLUS.svg';
-import flogo from '../../doc/img/logo/footer_logo.png';
-import FooterNewsCategories from "../FooterNewsCategories";
-const history = createBrowserHistory({forceRefresh:true});
+import React from 'react';
+import PropTypes from 'prop-types';
+import FooterCopyright from '../FooterCopyright';
+import FontAwesome from '../uiStyle/FontAwesome';
+import { ReactComponent as GtLogo } from '../../doc/img/gt-logo/logo-gt.svg';
+import { ReactComponent as GtLogo2 } from '../../doc/img/gt-logo/GT-PLUS.svg';
 
-const FooterArea = ({className}) => {
-    const [email, setEmail] = useState('');
-    const submitHandler = e => {
-        e.preventDefault();
-        setEmail('')
-    };
+const FooterArea = ({ className }) => {
     return (
-        <div className={`footer footer_area1 ${className ? className : ''}`}>
+        <div className={`footer footer_area1 ${className || ''}`}>
             <div className="container">
                 <div className="cta">
                     <div className="row">
                         <div className="col-8 col-md-7 ">
                             <div className="social2 partner_logos">
-                            <ul className="inline">
-                                <li> <GtLogo className="gt_logo"/></li>
-                                <li><GtLogo2 className="gt_logo"/></li>
-                                <li><img className="gt_logo" src="https://www.regio-health.de/cms/wp-content/uploads/2020/10/regiohealth-b.svg"/></li>
-                                <li><img className="gt_logo" src="https://www.regio-health.de/cms/wp-content/uploads/2020/10/regiohealth-k.svg"/></li>
-                                <li><img className="gt_logo" src="https://www.regio-health.de/cms/wp-content/uploads/2020/09/LOGO-WF.svg"/></li>
-                                <li><img className="gt_logo" src="https://www.gesundheitsticket.de/woak-logo.svg"/></li>
-                            </ul>
-                            </div>
-                            {/* <div className="row">
-                                <div className="col-md-6">
-                                    <div className="footer_logo logo">
-                                        <GtLogo className="gt_logo" onClick={() => history.push('/')} />
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="footer_logo logo">
-                                        <GtLogo2 className="gt_logo" onClick={() => history.push('/')} />
-                                    </div>
-                                </div>
-                            </div> */}
-                        </div>
-                        <div className="col-5 col-md-5 align-end">
-                        <div className="social2">
                                 <ul className="inline">
-                                    <li><a href="https://twitter.com/gesundticket" target="_blank"><FontAwesome name="twitter"/></a></li>
-                                    <li><a href="https://www.facebook.com/GesundheitsTicket" target="_blank"><FontAwesome name="facebook-f"/></a></li>
-                                    <li><a href="https://www.instagram.com/gesundheitsticket/" target="_blank"><FontAwesome name="instagram"/></a></li>
+                                    <li>
+                                        <GtLogo className="gt_logo" />
+                                    </li>
+                                    <li>
+                                        <GtLogo2 className="gt_logo" />
+                                    </li>
+                                    <li>
+                                        <img
+                                            alt="logo"
+                                            className="gt_logo"
+                                            src="https://www.regio-health.de/cms/wp-content/uploads/2020/10/regiohealth-b.svg"
+                                        />
+                                    </li>
+                                    <li>
+                                        <img
+                                            alt="logo"
+                                            className="gt_logo"
+                                            src="https://www.regio-health.de/cms/wp-content/uploads/2020/10/regiohealth-k.svg"
+                                        />
+                                    </li>
+                                    <li>
+                                        <img
+                                            alt="logo"
+                                            className="gt_logo"
+                                            src="https://www.regio-health.de/cms/wp-content/uploads/2020/09/LOGO-WF.svg"
+                                        />
+                                    </li>
+                                    <li>
+                                        <img
+                                            alt="logo"
+                                            className="gt_logo"
+                                            src="https://www.gesundheitsticket.de/woak-logo.svg"
+                                        />
+                                    </li>
                                 </ul>
                             </div>
                         </div>
-                        {/* <div className="col-md-6 col-lg-4 offset-lg-2 align-self-center">
-                            <div className="signup_form">
-                                <form onSubmit={submitHandler}>
-                                    <input onChange={e => setEmail(e.target.value)} value={email} className="signup"
-                                           type="email" placeholder="Your email address"/>
-                                    <button type="submit" className="cbtn">sign up</button>
-                                </form>
-                                <p>We hate spam as much as you do</p>
+                        <div className="col-5 col-md-5 align-end">
+                            <div className="social2">
+                                <ul className="inline">
+                                    <li>
+                                        <a
+                                            href="https://twitter.com/gesundticket"
+                                            target="_blank"
+                                        >
+                                            <FontAwesome name="twitter" />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.facebook.com/gesundheitsticket"
+                                            target="_blank"
+                                        >
+                                            <FontAwesome name="facebook-f" />
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.instagram.com/gesundheitsticket/"
+                                            target="_blank"
+                                        >
+                                            <FontAwesome name="instagram" />
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
-                        </div> */}
+                        </div>
                     </div>
                 </div>
-                {/* <div className="border_white"/>
-                <div className="space-40"/> */}
-                {/* <div className="row justify-content-center">
-                    <div className="col-lg-8">
-                        <div className="row">
-                            <div className="col-sm-6 col-lg">
-                                <div className="single_footer_nav border_white_right">
-                                    <FooterNewsCategories/>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-lg">
-                                <div className="single_footer_nav">
-                                    <h3 className="widget-title2">Living</h3>
-                                    <div className="row">
-                                        <div className="col-lg-6">
-                                            <ul>
-                                                <li><Link to="/">Crossword</Link>
-                                                </li>
-                                                <li><Link to="/">Food</Link>
-                                                </li>
-                                                <li><Link to="/">Automobiles</Link>
-                                                </li>
-                                                <li><Link to="/">Education</Link>
-                                                </li>
-                                                <li><Link to="/">Health</Link>
-                                                </li>
-                                                <li><Link to="/">Magazine</Link>
-                                                </li>
-                                                <li><Link to="/">Weddings</Link>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div className="col-lg-6">
-                                            <ul>
-                                                <li><Link to="/">Classifieds</Link>
-                                                </li>
-                                                <li><Link to="/">Photographies</Link>
-                                                </li>
-                                                <li><Link to="/">NYT Store</Link>
-                                                </li>
-                                                <li><Link to="/">Journalisms</Link>
-                                                </li>
-                                                <li><Link to="/">Public Editor</Link>
-                                                </li>
-                                                <li><Link to="/">Tools & Services</Link>
-                                                </li>
-                                                <li><Link to="/">My Account</Link>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="space-40"/>
-                        <div className="border_white"/>
-                        <div className="space-40"/>
-                        <div className="row">
-                            <div className="col-sm-6 col-lg-5">
-                                <div className="single_footer_nav border_white_right">
-                                    <h3 className="widget-title2">Opinion</h3>
-                                    <div className="row">
-                                        <div className="col-lg-6">
-                                            <ul>
-                                                <li><Link to="/">Today’s Opinion</Link>
-                                                </li>
-                                                <li><Link to="/">Op-Ed Contributing</Link>
-                                                </li>
-                                                <li><Link to="/">Contributing Writers</Link>
-                                                </li>
-                                                <li><Link to="/">Business News</Link>
-                                                </li>
-                                                <li><Link to="/">Collections</Link>
-                                                </li>
-                                                <li><Link to="/">Today’s Paper</Link>
-                                                </li>
-                                                <li><Link to="/">Saturday Review</Link>
-                                                </li>
-                                                <li><Link to="/">Product Review</Link>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 col-lg-7">
-                                <TwitterFeed/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4">
-                        <FooterMoreNews/>
-                    </div>
-                </div> */}
             </div>
-            <FooterCopyright/>
+            <FooterCopyright />
         </div>
     );
+};
+
+FooterArea.propTypes = {
+    className: PropTypes.string,
+};
+
+FooterArea.defaultProps = {
+    className: '',
 };
 
 export default FooterArea;
--- src/components/PostGallery/index.js
+++ src/components/PostGallery/index.js
@@ -1,158 +1,121 @@
-import React, {Component} from 'react';
-import {Link} from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
-import WidgetTab from "../WidgetTab";
-import Slider from "react-slick";
-import "../../../node_modules/slick-carousel/slick/slick.css";
-import FontAwesome from "../uiStyle/FontAwesome";
-import ModalVideo from 'react-modal-video'
-import PostCarousel from "../../components/PostCarousel";
-import TrendingNews from "../../components/TrendingNews";
-// images
+import WidgetTab from '../WidgetTab';
+import '../../../node_modules/slick-carousel/slick/slick.css';
+import PostCarousel from '../PostCarousel';
+import TrendingNews from '../TrendingNews';
 import sliderImg1 from '../../doc/img/header/sider-top.jpg';
-
 import './style.scss';
-const CMS_LINK = "https://cms.gesundheitsticket.de";
 
+const CMS_LINK = 'https://cms.gesundheitsticket.de';
 
-function SampleNextArrow(props) {
-    const {className, onClick} = props;
+const PostGallery = ({ className, latestArticles, topArticles, adsHome }) => {
     return (
-        <div onClick={onClick} className={`${className} slider_arrow arrow_right slick-arrow`}>
-            <FontAwesome name="angle-right"/>
-        </div>
-    );
-}
-
-function SamplePrevArrow(props) {
-    const {className, onClick} = props;
-    return (
-        <div onClick={onClick} className={`${className} slider_arrow arrow_left slick-arrow`}>
-            <FontAwesome name="angle-left"/>
-        </div>
-    );
-}
-
-class PostGallery extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            nav1: null,
-            nav2: null,
-            vModal: false,
-            videoId: '0r6C3z3TEKw'
-        };
-    }
-
-    componentDidMount() {
-        this.setState({
-            nav1: this.slider1,
-            nav2: this.slider2
-        });
-    }
-
-    modalHandler = (value) => {
-        this.setState({
-            vModal: value
-        })
-    };
-
-    render() {
-        const {className, latestArticles, topArticles, adsHome} = this.props;
-        const {nav1, nav2, vModal, videoId} = this.state;
-        const navSettings = {
-            nextArrow: <SampleNextArrow/>,
-            prevArrow: <SamplePrevArrow/>,
-            slidesToShow: 8,
-            swipeToSlide: true,
-            focusOnSelect: true,
-            centerMode: true,
-            responsive: [
-                {
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 8,
-                    }
-                },
-                {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 5,
-                    }
-                },
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-            ]
-        };
-        return (
-            <div className={`post_gallary_area mb40 ${className}`}>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                        <PostCarousel latestArticles={latestArticles.slice(0, 9)}  className="fifth_bg"/>
-                            <div className="row">
-                                <div className="col-xl-8">
-                                    <div className="slider_demo2">
-                                        {/* <Slider
-                                            asNavFor={nav2}
-                                            arrows={false}
-                                            fade={true}
-                                            ref={slider => (this.slider1 = slider)}
-                                        > */}
-                                            {topArticles.slice(0, 1).map((item, i) => (
-                                                <div key={i} className="single_post post_type6 xs-mb30">
-                                                     <div className="single_post_text">
-                                                        <h4>
-                                                            <Link className="play_btn" to={`/article/${item._id}`}>{item.title}</Link>
-                                                        </h4>
-                                                        <div className="meta meta_separator1">
-                                                            <Link to="#">{item.categories.length > 0 ? item.categories[0].name : ''}</Link>
-                                                            <Link to="#">{moment(item.createdAt).format("LL")}</Link>
-                                                        </div>
-                                                        <div className="space-10"/>
-                                                        <p className="post-p">{item.description}</p>
-                                                    </div>
-                                                    <div className="post_img gradient1">
-                                                        <img src={item.main_image ? `${CMS_LINK + item.main_image.url}` : sliderImg1} alt="thumb"/>
-                                                        {/* <span onClick={() => this.modalHandler(true)}
-                                                              className="tranding"><FontAwesome
-                                                            name="play"/></span> */}
-                                                    </div>
-                                                </div>
-                                            ))}
-                                        {/* </Slider> */}
-                                    </div>
-                                    <div className="slider_demo1">
-                                        {/* <Slider
-                                            ref={slider => (this.slider2 = slider)}
-                                            asNavFor={nav1}
-                                            {...navSettings}
+        <div className={`post_gallary_area mb40 ${className}`}>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <PostCarousel
+                            latestArticles={latestArticles.slice(0, 9)}
+                            className="fifth_bg"
+                        />
+                        <div className="row">
+                            <div className="col-xl-8">
+                                <div className="slider_demo2">
+                                    {topArticles.slice(0, 1).map((item) => (
+                                        <div
+                                            key={item.id}
+                                            className="single_post post_type6 xs-mb30"
                                         >
-                                            {thumbs.slice(0, 9).map((item, i) => (
-                                                <div key={i} className="single_gallary_item">
-                                                    <img src={item} alt="thumb"/>
+                                            <div className="single_post_text">
+                                                <h4>
+                                                    <Link
+                                                        className="play_btn"
+                                                        to={`/article/${item.id}`}
+                                                    >
+                                                        {item.title}
+                                                    </Link>
+                                                </h4>
+                                                <p className="post-p">
+                                                    {item.description}
+                                                </p>
+                                                <div className="meta meta_separator1 d-flex">
+                                                    <p>
+                                                        {item.categories
+                                                            .length > 0
+                                                            ? item.categories[0]
+                                                                  .name
+                                                            : ''}
+                                                    </p>
+                                                    <p>
+                                                        {moment(
+                                                            item.createdAt
+                                                        ).format('LL')}
+                                                    </p>
                                                 </div>
-                                            ))}
-                                        </Slider> */}
-                                    </div>
-                                     <TrendingNews adsHome={adsHome.filter((ad)=> ad.size === "s729x90")} latestArticles={latestArticles.slice(9, 18)}/>
+                                                <div className="space-10" />
+                                            </div>
+                                            <Link to={`/article/${item.id}`}>
+                                                <div className="post_img gradient1">
+                                                    <img
+                                                        src={
+                                                            item.main_image
+                                                                ? `${
+                                                                      CMS_LINK +
+                                                                      item
+                                                                          .main_image
+                                                                          .url
+                                                                  }`
+                                                                : sliderImg1
+                                                        }
+                                                        alt="thumb"
+                                                    />
+                                                </div>
+                                            </Link>
+                                        </div>
+                                    ))}
                                 </div>
-                                <div className="col-xl-4">
-                                    <WidgetTab adsHome={adsHome.filter((ad)=> ad.size === "s350x292")} latestArticles={latestArticles.slice(18, 23)} dark={true}/>
-                                </div>
+                                <TrendingNews
+                                    adsHome={adsHome.filter(
+                                        (ad) => ad.size === 's729x90'
+                                    )}
+                                    latestArticles={latestArticles.slice(9, 18)}
+                                />
+                            </div>
+                            <div className="col-xl-4">
+                                <WidgetTab
+                                    adsHome={adsHome.filter(
+                                        (ad) => ad.size === 's350x292'
+                                    )}
+                                    latestArticles={latestArticles.slice(
+                                        18,
+                                        23
+                                    )}
+                                    dark
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* <ModalVideo channel='youtube' isOpen={vModal} videoId={videoId}
-                            onClose={() => this.modalHandler(false)}/> */}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+PostGallery.propTypes = {
+    className: PropTypes.string,
+    latestArticles: PropTypes.arrayOf(PropTypes.object),
+    topArticles: PropTypes.arrayOf(PropTypes.object),
+    adsHome: PropTypes.arrayOf(PropTypes.object),
+};
+
+PostGallery.defaultProps = {
+    className: '',
+    latestArticles: [],
+    topArticles: [],
+    adsHome: [],
+};
 
 export default PostGallery;
