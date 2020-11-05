ef4ef5e4ff664a831a23ecba046969f2b5b3739b
maximilianofried <maximilianofried@gmail.com>
2020-11-05 16:35:02 +0100

    styles to searchbox

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -61,45 +61,53 @@ const SearchBox = ({
         <form onSubmit={handleSubmit}>
             <Combobox
                 onSelect={(address) => onComboSelect(address)}
-                className="d-flex align-items-center my-flex-container"
+                className="form-row align-items-center"
             >
-                <Listbox type={type} onChange={setType}>
-                    <ListboxOption value="alle">Alle</ListboxOption>
-                    <ListboxOption value="article">Articles</ListboxOption>
-                    <ListboxOption value="offer">Offers</ListboxOption>
-                </Listbox>
-                <ComboboxInput
-                    value={value}
-                    onChange={(e) => {
-                        setValue(e.target.value);
-                    }}
-                    disabled={!ready}
-                    placeholder="PLZ oder Ort"
-                />
-                <ComboboxPopover className="pop_over">
-                    {status === 'OK' &&
-                        data.map((result) => {
-                            return (
-                                <ComboboxOption
-                                    key={result.place_id}
-                                    value={result.description}
-                                />
-                            );
-                        })}
-                </ComboboxPopover>
-                <Listbox radius={radius} onChange={setRadius}>
-                    <ListboxOption value="5000">+5 km</ListboxOption>
-                    <ListboxOption value="10000">+10 km</ListboxOption>
-                    <ListboxOption value="20000">+20 km</ListboxOption>
-                    <ListboxOption value="30000">+30 km</ListboxOption>
-                    <ListboxOption value="50000">+50 km</ListboxOption>
-                    <ListboxOption value="100000">+100 km</ListboxOption>
-                    <ListboxOption value="150000">+150 km</ListboxOption>
-                    <ListboxOption value="200000">+200 km</ListboxOption>
-                </Listbox>
-                <button type="submit" className="btn-sm">
-                    <i className="fa fa-search" /> Finden
-                </button>
+                <div className="col-auto">
+                    <Listbox type={type} onChange={setType}>
+                        <ListboxOption value="alle">Alle</ListboxOption>
+                        <ListboxOption value="article">Articles</ListboxOption>
+                        <ListboxOption value="offer">Offers</ListboxOption>
+                    </Listbox>
+                </div>
+                <div className="col-auto">
+                    <ComboboxInput
+                        value={value}
+                        onChange={(e) => {
+                            setValue(e.target.value);
+                        }}
+                        disabled={!ready}
+                        placeholder="PLZ oder Ort"
+                    />
+                    <ComboboxPopover className="pop_over">
+                        {status === 'OK' &&
+                            data.map((result) => {
+                                return (
+                                    <ComboboxOption
+                                        key={result.place_id}
+                                        value={result.description}
+                                    />
+                                );
+                            })}
+                    </ComboboxPopover>
+                </div>
+                <div className="col-auto">
+                    <Listbox radius={radius} onChange={setRadius}>
+                        <ListboxOption value="5000">+5 km</ListboxOption>
+                        <ListboxOption value="10000">+10 km</ListboxOption>
+                        <ListboxOption value="20000">+20 km</ListboxOption>
+                        <ListboxOption value="30000">+30 km</ListboxOption>
+                        <ListboxOption value="50000">+50 km</ListboxOption>
+                        <ListboxOption value="100000">+100 km</ListboxOption>
+                        <ListboxOption value="150000">+150 km</ListboxOption>
+                        <ListboxOption value="200000">+200 km</ListboxOption>
+                    </Listbox>
+                </div>
+                <div className="col-auto">
+                    <button type="submit" className="btn-sm search-button">
+                        <i className="fa fa-search" /> Finden
+                    </button>
+                </div>
             </Combobox>
         </form>
     );
