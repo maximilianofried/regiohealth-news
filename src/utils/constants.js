bb5fbfe594269734c84feb5087de89c1eb594ad3
maximiliano-gt <maximiliano@gesundheitsticket.de>
2021-01-14 14:48:08 +0100

    updates constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 export const RG_LOGO =
     'https://www.regio-health.de/cms/wp-content/uploads/2020/10/regiohealth-news.svg';
-export const RG_DESCRIPTION = `Regio Health News platform was developed to give our health partners the opportunity to publish their current and regional offers. Be it a new course start, a monthly offer or a discount campaign.
-Regio Health News is also an information platform on the many aspects of health prevention, also beyond Germany, and thus opens up a broader view beyond the edges of the plates into the world.`;
+export const RG_DESCRIPTION = `RegioHealth.News ist eine Plattform für Informationen und Neuigkeiten rund um regionale und überregionale Gesundheitsthemen der Partner des GesundheitsTicket-Netzwerks.
+Die Idee für RegioHealth.News entstand aus dem Wunsch einer besseren Vernetzung der Kunden und GesundheitsPartnern des Netzwerks der GesundheitsTicket GmbH mit der Möglichkeit, über Projekte und Aktionen und Angebote zu publizieren, um sie der Öffentlichkeit aber auch konkreten Zielgruppen wie GesundheitsTicket-Inhabern zugänglich zu machen.
+Darüber hinaus veröffentlicht die Plattform auch Informationen rund um Aktivitäten und Projekte unserer Unternehmensgruppe und stellt allgemein interessante Informationen zum Thema Gesundheit und gesunde Lebenswelten zur Verfügung.
+Damit ist RegioHealth.News auch eine geeignete Informationsquelle für Unternehmen, Kommunen, Gesundheitsregionen, GesundheitsmanagerInnen, PersonalleiterInnen und alle an Gesundheitsthemen interessierte Menschen.`;
