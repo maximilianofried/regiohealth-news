77505b9fd1ae89a7bb83cb6020a3f7b344e9efc2
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-05-27 11:18:32 +0200

    updates

--- src/components/BannerSection/index.js
+++ src/components/BannerSection/index.js
@@ -14,7 +14,7 @@ const BannerSection = ({ className, fetchAds, adsCategory }) => {
         <div className={`${className || 'padding2020 '}`}>
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-8 m-auto">
+                    <div className="col-lg-12 m-auto">
                         <div className="banner-section">
                             <a href={banner729x90.link} target="_blank">
                                 {banner729x90.image &&
