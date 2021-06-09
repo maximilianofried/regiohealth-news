2dc5ec8ea50276d04cf2a266e6f7635d38654066
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-06-09 13:03:14 +0200

    updates

--- src/components/PostGalleryTwo/index.js
+++ src/components/PostGalleryTwo/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { isMobileOnly } from 'react-device-detect';
 import moment from 'moment';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import FontAwesome from '../uiStyle/FontAwesome';
@@ -22,6 +23,7 @@ const PostGalleryTwo = ({
     const displayOffersHomepage = latestOffers.some(
         (offer) => offer.end > moment().format('YYYY-MM-DD')
     );
+    console.log('isMobile', isMobileOnly);
     return (
         <div className="post_gallary_area theme3_bg mb40 padding-top-30">
             <div className="container">
@@ -81,30 +83,32 @@ const PostGalleryTwo = ({
                             </Link>
                         )}
                         <div className="space-10" />
-                        <div className="banner2 border-radious5 banner_horizontal mb20 mt20">
-                            <iframe
-                                title="adserver-horizontal"
-                                id="abadaf92"
-                                name="abadaf92"
-                                src="https://adserver.gesundheitsticket.de/revive/www/delivery/afr.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-                                frameBorder="0"
-                                scrolling="no"
-                                width="729"
-                                height="90"
-                                allow="autoplay"
-                            >
-                                <a
-                                    href="https://adserver.gesundheitsticket.de/revive/www/delivery/ck.php?n=ad79cc61&amp;cb=INSERT_RANDOM_NUMBER_HERE"
-                                    target="_blank"
+                        {!isMobileOnly && (
+                            <div className="banner2 border-radious5 banner_horizontal mb20 mt20">
+                                <iframe
+                                    title="adserver-horizontal"
+                                    id="abadaf92"
+                                    name="abadaf92"
+                                    src="https://adserver.gesundheitsticket.de/revive/www/delivery/afr.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+                                    frameBorder="0"
+                                    scrolling="no"
+                                    width="729"
+                                    height="90"
+                                    allow="autoplay"
                                 >
-                                    <img
-                                        src="https://adserver.gesundheitsticket.de/revive/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ad79cc61"
-                                        border="0"
-                                        alt=""
-                                    />
-                                </a>
-                            </iframe>
-                        </div>
+                                    <a
+                                        href="https://adserver.gesundheitsticket.de/revive/www/delivery/ck.php?n=ad79cc61&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src="https://adserver.gesundheitsticket.de/revive/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ad79cc61"
+                                            border="0"
+                                            alt=""
+                                        />
+                                    </a>
+                                </iframe>
+                            </div>
+                        )}
                         <BusinessNewsTwo
                             articleLimit={articleLimit}
                             publisherArticles={publisherArticles}
