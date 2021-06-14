fcb88a6f16b86e4dcbb689e4cbe8711764015f1f
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-06-14 09:38:26 +0200

    updates

--- src/components/AdserverIframe/index.js
+++ src/components/AdserverIframe/index.js
@@ -5,8 +5,8 @@ const AdserverIframe = () => {
         <div className="banner2 border-radious5 banner_homepage mb20 mt20 mt-lg-0">
             <iframe
                 title="adserver"
-                id="aa1b86cb"
-                name="aa1b86cb"
+                id="a64f1ca8"
+                name="a64f1ca8"
                 src="https://adserver.gesundheitsticket.de/revive/www/delivery/afr.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE"
                 frameBorder="0"
                 scrolling="no"
@@ -15,11 +15,11 @@ const AdserverIframe = () => {
                 allow="autoplay"
             >
                 <a
-                    href="https://adserver.gesundheitsticket.de/revive/www/delivery/ck.php?n=a4d71c93&amp;cb=INSERT_RANDOM_NUMBER_HERE"
+                    href="https://adserver.gesundheitsticket.de/revive/www/delivery/ck.php?n=a75df846&amp;cb=INSERT_RANDOM_NUMBER_HERE"
                     target="_blank"
                 >
                     <img
-                        src="https://adserver.gesundheitsticket.de/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a4d71c93"
+                        src="https://adserver.gesundheitsticket.de/revive/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a75df846"
                         border="0"
                         alt=""
                     />
