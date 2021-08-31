d0e9249a89a38b4430a5f95b899fb2fb64fe2636
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-08-31 18:15:03 +0200

    adds new cookiefirst

--- src/containers/CookieDeclarationPage/index.js
+++ src/containers/CookieDeclarationPage/index.js
@@ -6,19 +6,19 @@ import { fetchPage } from '../../store/actions';
 import Metadata from '../../components/Metadata';
 
 const CookieDeclarationPage = ({ fetchPage, page, name }) => {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.id = 'CookieDeclaration';
-        script.src =
-            'https://consent.cookiebot.com/8fa59645-db52-4940-913b-5e5a06a99e9f/cd.js';
-        script.type = 'text/javascript';
-        script.async = true;
-        const div = document.getElementsByClassName('author_about')[0];
-        div.appendChild(script);
-        return () => {
-            div.removeChild(script);
-        };
-    }, []);
+    // useEffect(() => {
+    //     const script = document.createElement('script');
+    //     script.id = 'CookieDeclaration';
+    //     script.src =
+    //         'https://consent.cookiebot.com/8fa59645-db52-4940-913b-5e5a06a99e9f/cd.js';
+    //     script.type = 'text/javascript';
+    //     script.async = true;
+    //     const div = document.getElementsByClassName('author_about')[0];
+    //     div.appendChild(script);
+    //     return () => {
+    //         div.removeChild(script);
+    //     };
+    // }, []);
     return (
         <>
             <Metadata
@@ -26,17 +26,23 @@ const CookieDeclarationPage = ({ fetchPage, page, name }) => {
                 description={name}
                 url={`${process.env.REACT_APP_BASE_PAGE_URL}/${name}`}
             />
-            <BreadCrumb className="shadow5" title="Cookie Erklärung">
-                <>
-                    <div className="space-50" />
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="author_about" />
-                        </div>
-                    </div>
-                </>
-            </BreadCrumb>
+            <div id="cookiefirst-policy-page" />
+            <div>
+                This cookie policy has been created and updated by{' '}
+                <a href="https://cookiefirst.com">CookieFirst.com</a>.
+            </div>
+
             <div className="space-70" />
+
+            <div id="cookiefirst-cookies-table" />
+            <div>
+                This cookie table has been created and updated by the{' '}
+                <a href="https://cookiefirst.com">
+                    CookieFirst consent management platform
+                </a>
+                .
+            </div>
+
             <BannerSection />
         </>
     );
