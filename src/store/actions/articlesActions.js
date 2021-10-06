f6faa981d550d7e1b2a26a0c76783e07ed998a6e
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-10-06 10:57:48 +0200

    allows any content in publisher and news section homepage

--- src/store/actions/articlesActions.js
+++ src/store/actions/articlesActions.js
@@ -98,8 +98,8 @@ export const fetchArticles = ({
 export const fetchArticleHomepage = () => {
     return (dispatch) => {
         const urlMainArticle = `${process.env.REACT_APP_CMS_URL}/contents/published?_sort=publishAt:desc&&homepage=main_article`;
-        const urlPublisher = `${process.env.REACT_APP_CMS_URL}/contents/published?_sort=publishAt:desc&type=article&homepage=publisher`;
-        const urlNews = `${process.env.REACT_APP_CMS_URL}/contents/published?_sort=publishAt:desc&type=article&homepage=news&_limit=5`;
+        const urlPublisher = `${process.env.REACT_APP_CMS_URL}/contents/published?_sort=publishAt:desc&homepage=publisher`;
+        const urlNews = `${process.env.REACT_APP_CMS_URL}/contents/published?_sort=publishAt:desc&homepage=news&_limit=5`;
         dispatch(fetchArticlesRequest);
         axios
             .all([
