ff3670a41e2e33e1a1aaa62d6a122b7a06a2621d
Fried Maximiliano <maximiliano.fried@gmail.com>
2021-10-19 15:20:20 +0200

    solves bugs

--- src/store/reducers/newsReducer.js
+++ src/store/reducers/newsReducer.js
@@ -40,7 +40,7 @@ const articlesNewsReducer = (state = initialState, action) => {
         case UPDATE_NEWS_PAGE_START_PARAM:
             return {
                 ...state,
-                start: state.start + 2,
+                start: state.start + 6,
             };
         default:
             return state;
