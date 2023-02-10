36505b2710c073c28e8e0b02039251f43d617937
maximiliano-gt <maximiliano@gesundheitsticket.de>
2023-02-10 13:07:16 +0100

    adds half banner

---
+++ src/components/AdserverIframe/HalfBanner/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const HalfBanner = () => {
+    return (
+        <div className="banner2 border-radious5 banner_horizontal mb20 mt20">
+            <iframe
+                title="halfBanner"
+                id="ac1cacf8"
+                name="ac1cacf8"
+                src="http://adserver.gesundheitsticket.de/revive/www/delivery/afr.php?zoneid=16&amp;target=_blank&amp;cb=234"
+                width="234"
+                height="60"
+                allow="autoplay"
+            >
+                <a
+                    href="http://adserver.gesundheitsticket.de/revive/www/delivery/ck.php?n=a97333e9&amp;cb=234"
+                    target="_blank"
+                >
+                    <img
+                        src="http://adserver.gesundheitsticket.de/revive/www/delivery/avw.php?zoneid=16&amp;cb=234&amp;n=a97333e9"
+                        border="0"
+                        alt=""
+                    />
+                </a>
+            </iframe>
+        </div>
+    );
+};
+
+export default HalfBanner;
